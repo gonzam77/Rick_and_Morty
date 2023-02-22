9bc4eabe53d389cd70f7cefc2f904715ef6f66d1
gonzam77 <gonzam77@gmial.com>
2023-02-22 19:27:18 -0300

    modified

---
+++ Back/src/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+import "./utils/data"
+
+const PORT = 3001;
+
+// http.createServer((req, res) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     if(req.url.includes("rickandmorty/character"))
+
+
+// }).listen(PORT, "localhost")
+
+
+
+module.exports = null
